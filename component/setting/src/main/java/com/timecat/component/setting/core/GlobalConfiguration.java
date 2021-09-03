ed36e3d3edcecb161674aec56127a468cefccf36
linxueyuanstdio <761516186@qq.com>
2021-09-03 12:02:09 +0800

    s

--- component/setting/src/main/java/com/timecat/component/setting/core/GlobalConfiguration.java
+++ component/setting/src/main/java/com/timecat/component/setting/core/GlobalConfiguration.java
@@ -3,15 +3,15 @@ package com.timecat.component.setting.core;
 import android.app.Application;
 import android.content.Context;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.FragmentManager;
-
 import com.jess.arms.base.delegate.AppLifecycles;
 import com.jess.arms.di.module.GlobalConfigModule;
 import com.jess.arms.integration.ConfigModule;
 import com.tencent.mmkv.MMKV;
 
 import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
 
 /**
  * ================================================ CommonSDK 的 GlobalConfiguration
@@ -26,11 +26,11 @@ import java.util.List;
 public class GlobalConfiguration implements ConfigModule {
 
     @Override
-    public void applyOptions(Context context, GlobalConfigModule.Builder builder) {
+    public void applyOptions(@NonNull Context context, @NonNull GlobalConfigModule.Builder builder) {
     }
 
     @Override
-    public void injectAppLifecycle(Context context, List<AppLifecycles> lifecycles) {
+    public void injectAppLifecycle(@NonNull Context context, List<AppLifecycles> lifecycles) {
         lifecycles.add(0, new AppLifecycles() {
 
             @Override
@@ -50,23 +50,14 @@ public class GlobalConfiguration implements ConfigModule {
     }
 
     @Override
-    public void injectActivityLifecycle(Context context,
-                                        List<Application.ActivityLifecycleCallbacks> lifecycles) {
+    public void injectActivityLifecycle(@NonNull Context context,
+            @NonNull List<Application.ActivityLifecycleCallbacks> lifecycles) {
     }
 
     @Override
-    public void injectFragmentLifecycle(Context context,
-                                        List<FragmentManager.FragmentLifecycleCallbacks> lifecycles) {
-//        lifecycles.add(new FragmentManager.FragmentLifecycleCallbacks() {
-//
-//            @Override
-//            public void onFragmentCreated(FragmentManager fm, Fragment f, Bundle savedInstanceState) {
-//                // 在配置变化的时候将这个 Fragment 保存下来,在 Activity 由于配置变化重建是重复利用已经创建的Fragment。
-//                // https://developer.android.com/reference/android/app/Fragment.html?hl=zh-cn#setRetainInstance(boolean)
-//                // 在 Activity 中绑定少量的 Fragment 建议这样做,如果需要绑定较多的 Fragment 不建议设置此参数,如 ViewPager 需要展示较多 Fragment
-//                f.setRetainInstance(true);
-//            }
-//        });
+    public void injectFragmentLifecycle(@NonNull Context context, @NonNull List<FragmentManager.FragmentLifecycleCallbacks> lifecycles) {
+
     }
+
 
 }
